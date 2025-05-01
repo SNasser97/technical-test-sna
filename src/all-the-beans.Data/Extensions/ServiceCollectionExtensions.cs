19269e2c7c1e85a6b25c241ce6e33e4ff32d4e8e
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-01 18:59:56 +0100

    feat: update db context reference

--- src/all-the-beans.Data/Extensions/ServiceCollectionExtensions.cs
+++ src/all-the-beans.Data/Extensions/ServiceCollectionExtensions.cs
@@ -11,16 +11,13 @@ namespace all_the_beans.Data.Extensions
         {
             // This allows us to register without exposing the coffee bean Db context class in the api layer.
             // We can use the private extension method for registering any future Db contexts.
-            return services.AddDbContext<CoffeeBeanContext>();
+            return services.AddDbContext<CoffeeBeanDbContext>();
         }
 
         public static IServiceCollection AddCoffeeBeanRepositories(this IServiceCollection services)
         {
-            // This allows us to register without exposing the coffee bean Db context class in the api layer.
-            // We can use the private extension method for registering any future Db contexts.
             services.AddScoped<ICoffeeBeanReadRepository, CoffeeBeanReadRepository>();
             services.AddScoped<ICoffeeBeanWriteRepository, CoffeeBeanWriteRepository>();
-
             return services;
         }
     }
