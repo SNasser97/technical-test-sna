37e34bb2d495e0e9860aa25f81950743e7a4c974
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-02 20:30:57 +0100

    Refactor: Update namespace for GetCoffeeBeansQuery class
    The namespace of the `GetCoffeeBeansQuery` class was updated
    from `all_the_beans.Logic.Queries.V1.CoffeeBean.GetCoffeeBeansQuery`
    to `all_the_beans.Logic.Queries.V1.CoffeeBeanQueries.GetCoffeeBeansQuery`.
    
    This change improves the organization and categorization of
    query classes by grouping them under `CoffeeBeanQueries`.

--- src/all-the-beans.Logic/Queries/V1/CoffeeBeanQueries/GetCoffeeBeansQuery/GetCoffeeBeansQuery.cs
+++ src/all-the-beans.Logic/Queries/V1/CoffeeBeanQueries/GetCoffeeBeansQuery/GetCoffeeBeansQuery.cs
@@ -1,6 +1,6 @@
 ﻿using all_the_beans.Entities.Queries.V1.GetCoffeeBeansQuery;
 
-namespace all_the_beans.Logic.Queries.V1.CoffeeBean.GetCoffeeBeansQuery
+namespace all_the_beans.Logic.Queries.V1.CoffeeBeanQueries.GetCoffeeBeansQuery
 {
     public class GetCoffeeBeansQuery : IGetCoffeeBeansQuery
     {
