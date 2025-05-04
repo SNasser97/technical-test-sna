e798a88fcfc6b184990458cacec81d668c58dd30
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-04 21:01:42 +0100

    feat: add ServiceProviderExtension methods class

---
+++ src/all-the-beans.Tests/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace all_the_beans.Tests.Extensions
+{
+    internal static class ServiceProviderExtensions
+    {
+        /// <summary>
+        /// Performs DbContext Action
+        /// </summary>
+        /// <typeparam name="TDbContext">The db context</typeparam>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <param name="dbContextAction">The db context action method to execute against the db context</param>
+        public static async Task<IServiceProvider> PerformDbContextActionAsync<TDbContext>(this IServiceProvider serviceProvider, Func<TDbContext,Task> dbContextAction)
+            where TDbContext : DbContext
+        {
+            using (IServiceScope serviceScope = serviceProvider.CreateScope())
+            {
+                TDbContext coffeeBeanDbContext = serviceScope.ServiceProvider.GetRequiredService<TDbContext>();
+                await dbContextAction(coffeeBeanDbContext);
+            }
+
+            return serviceProvider;
+        }
+    }
+}
