9ac315f03ea7d93e2b6fa4170c7457a182ea76a4
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-04 19:35:59 +0100

    feat: add coffeebean db context to repository classes

--- src/all-the-beans.Data/Repositories/CoffeeBeanRepository/CoffeeBeanWriteRepository.cs
+++ src/all-the-beans.Data/Repositories/CoffeeBeanRepository/CoffeeBeanWriteRepository.cs
@@ -1,20 +1,31 @@
-﻿using all_the_beans.Entities.Entity.CoffeeBean;
+﻿using all_the_beans.Data.Context;
+using all_the_beans.Entities.Entity.CoffeeBean;
 using all_the_beans.Entities.Repositories.CoffeeBeanRepository;
 
 namespace all_the_beans.Data.Repositories.CoffeeBeanRepository
 {
     internal class CoffeeBeanWriteRepository : ICoffeeBeanWriteRepository
     {
+        private readonly CoffeeBeanDbContext coffeeBeanDbContext;
+
+        public CoffeeBeanWriteRepository(CoffeeBeanDbContext coffeeBeanDbContext)
+        {
+            this.coffeeBeanDbContext = coffeeBeanDbContext;
+        }
+
+        /// <inherit doc/>
         public Task CreateAsync(CoffeeBean coffeeBean)
         {
             throw new NotImplementedException();
         }
 
+        /// <inherit doc/>
         public Task DeleteAsync(string id)
         {
             throw new NotImplementedException();
         }
 
+        /// <inherit doc/>
         public Task UpdateAsync(CoffeeBean coffeeBean)
         {
             throw new NotImplementedException();
