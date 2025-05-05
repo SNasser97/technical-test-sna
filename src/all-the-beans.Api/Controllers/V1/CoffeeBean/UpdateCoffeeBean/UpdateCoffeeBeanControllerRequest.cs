2e4bb1286f931614f05feea5d0b691e05a0ea273
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-05 15:04:52 +0100

    fix: add regex pattern to controller request - UpdateCoffeeBean

--- src/all-the-beans.Api/Controllers/V1/CoffeeBean/UpdateCoffeeBean/UpdateCoffeeBeanControllerRequest.cs
+++ src/all-the-beans.Api/Controllers/V1/CoffeeBean/UpdateCoffeeBean/UpdateCoffeeBeanControllerRequest.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using all_the_beans.Api.Constants;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace all_the_beans.Api.Controllers.V1.CoffeeBean.UpdateCoffeeBean
 {
     public partial record UpdateCoffeeBeanControllerRequest
     {
         [FromRoute]
+        [RegularExpression(RegularExpressionConstants.requestIdPattern)]
         public string Id { get; set; }
 
         [FromBody]
