0eccfd691f861a71c129577e5a89085572ea79e1
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-05 15:26:57 +0100

    feat: make command request partial - UpdateCoffeeBean

--- src/all-the-beans.Entities/Commands/V1/CoffeeBeanCommands/UpdateCoffeeBeanCommand/UpdateCoffeeBeanCommandRequest.cs
+++ src/all-the-beans.Entities/Commands/V1/CoffeeBeanCommands/UpdateCoffeeBeanCommand/UpdateCoffeeBeanCommandRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace all_the_beans.Entities.Commands.V1.CoffeeBeanCommands.UpdateCoffeeBeanCommand
 {
-    public record UpdateCoffeeBeanCommandRequest(string Id, decimal Cost, string Image, string Colour, string Name, string Description, string Country)
+    public partial record UpdateCoffeeBeanCommandRequest(string Id, decimal Cost, string Image, string Colour, string Name, string Description, string Country)
     {
     }
 }
