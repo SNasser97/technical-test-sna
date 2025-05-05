04f7a75a25ec6430a5fc39f464cb55ac0bfb08bb
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-05 19:26:39 +0100

    feat: add coffebean read repository to query - GetCoffeeBeans

--- src/all-the-beans.Logic/Queries/V1/CoffeeBeanQueries/GetCoffeeBeansQuery/GetCoffeeBeansQuery.cs
+++ src/all-the-beans.Logic/Queries/V1/CoffeeBeanQueries/GetCoffeeBeansQuery/GetCoffeeBeansQuery.cs
@@ -1,12 +1,27 @@
-﻿using all_the_beans.Entities.Queries.V1.GetCoffeeBeansQuery;
+﻿using all_the_beans.Entities.Entity.CoffeeBean;
+using all_the_beans.Entities.Queries.V1.GetCoffeeBeansQuery;
+using all_the_beans.Entities.Repositories.CoffeeBeanRepository;
 
 namespace all_the_beans.Logic.Queries.V1.CoffeeBeanQueries.GetCoffeeBeansQuery
 {
     public class GetCoffeeBeansQuery : IGetCoffeeBeansQuery
     {
-        public Task<GetCoffeeBeansQueryResponse> ExecuteAsync(GetCoffeeBeansQueryRequest request)
+        private readonly ICoffeeBeanReadRepository coffeeBeanReadRepository;
+
+        public GetCoffeeBeansQuery(ICoffeeBeanReadRepository coffeeBeanReadRepository)
         {
-            throw new NotImplementedException();
+            this.coffeeBeanReadRepository = coffeeBeanReadRepository;
+        }
+
+        public async Task<GetCoffeeBeansQueryResponse> ExecuteAsync(GetCoffeeBeansQueryRequest request)
+        {
+            // considerations: validate page, validate items per page (not negative, not supplying large itemsPerPage)
+            //await this.coffeeBeanReadRepository.GetAsync(request.Page, request.ItemsPerPAge);
+
+            return new GetCoffeeBeansQueryResponse
+            {
+                CoffeeBeans = Enumerable.Empty<CoffeeBean>()
+            };
         }
     }
 }
