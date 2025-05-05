915d1045ba5e4059ed8d20141cd3834b08074a3d
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-05 20:01:50 +0100

    feat: update namespace

---
+++ src/all-the-beans.Api/Controllers/V1/CoffeeBeanControllers/GetCoffeeBeans/GetCoffeeBeansControllerRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace all_the_beans.Api.Controllers.V1.CoffeeBeanControllers.GetCoffeeBeans
+{
+    public partial record GetCoffeeBeansControllerRequest
+    {
+        [FromQuery]
+        public int Page { get; set; }
+
+        [FromQuery]
+        public int ItemsPerPage { get; set; }
+    }
+}
