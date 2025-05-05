bf78d52f18ad6c3c4bf5a18b74ec19122daa00bf
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-05 14:21:02 +0100

    feat: use regex pattern for controller request moel - DeleteCoffeeBean

--- src/all-the-beans.Api/Controllers/V1/CoffeeBean/DeleteCoffeeBean/DeleteCoffeeBeanControllerRequest.cs
+++ src/all-the-beans.Api/Controllers/V1/CoffeeBean/DeleteCoffeeBean/DeleteCoffeeBeanControllerRequest.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using all_the_beans.Api.Constants;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace all_the_beans.Api.Controllers.V1.CoffeeBean.DeleteCoffeeBean
 {
     public partial record DeleteCoffeeBeanControllerRequest
     {
         [FromRoute]
+        [RegularExpression(RegularExpressionConstants.requestIdPattern)]
         public string Id { get; set; }
     }
 }
