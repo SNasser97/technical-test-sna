8abfc152ae84b1c597fbbc15050900c17f7fdb31
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-06 17:57:43 +0100

    feat: add bean of the day query logic

---
+++ src/all-the-beans.Entities/Queries/V1/GetBeanOfTheDay/IGetBeanOfTheDayQuery.cs
@@ -0,0 +1,8 @@
+﻿using all_the_beans.Entities.Queries.Common;
+
+namespace all_the_beans.Entities.Queries.V1.GetBeanOfTheDay
+{
+    public interface IGetBeanOfTheDayQuery : IQuery<GetBeanOfTheDayQueryResponse>
+    {
+    }
+}
---
+++ src/all-the-beans.Logic/Queries/V1/CoffeeBeanQueries/GetBeanOfTheDay/GetBeanOfTheDayQuery.cs
@@ -0,0 +1,32 @@
+﻿using all_the_beans.Entities.Entity.CoffeeBean;
+using all_the_beans.Entities.Queries.V1.GetBeanOfTheDay;
+using all_the_beans.Entities.Repositories.CoffeeBeanRepository;
+
+namespace all_the_beans.Logic.Queries.V1.CoffeeBeanQueries.GetBeanOfTheDay
+{
+    public class GetBeanOfTheDayQuery : IGetBeanOfTheDayQuery
+    {
+        private readonly ICoffeeBeanReadRepository coffeeBeanReadRepository;
+
+        public GetBeanOfTheDayQuery(ICoffeeBeanReadRepository coffeeBeanReadRepository)
+        {
+            this.coffeeBeanReadRepository = coffeeBeanReadRepository;
+        }
+
+        public async Task<GetBeanOfTheDayQueryResponse> ExecuteAsync()
+        {
+            CoffeeBean coffeeBean =  await this.coffeeBeanReadRepository.GetBeanOfTheDayAsync();
+
+            return new GetBeanOfTheDayQueryResponse
+            {
+                Id = coffeeBean.Id,
+                Name = coffeeBean.Name,
+                Cost = coffeeBean.Cost,
+                Image = coffeeBean.Image,
+                Colour = coffeeBean.Colour,
+                Description = coffeeBean.Description,
+                Country = coffeeBean.Country
+            };
+        }
+    }
+}
