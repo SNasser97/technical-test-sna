9a7d831ff5a4b112145fd5962a75ad69c01e9a4a
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-06 17:56:31 +0100

    feat: add bean of the day controller

---
+++ src/all-the-beans.Api/Controllers/V1/CoffeeBeanControllers/GetBeanOfTheDay/GetBeanOfTheDayController.cs
@@ -0,0 +1,28 @@
+﻿using all_the_beans.Entities.Queries.V1.GetBeanOfTheDay;
+using Microsoft.AspNetCore.Mvc;
+
+namespace all_the_beans.Api.Controllers.V1.CoffeeBeanControllers.GetBeanOfTheDay
+{
+    /// <summary>
+    /// Gets BeanOfTheDay coffee bean.
+    /// </summary>
+    [Route("v1/api/coffeebeans/bean-of-the-day")]
+    [ApiController]
+    public class GetBeanOfTheDayController : ControllerBase
+    {
+        private readonly IGetBeanOfTheDayQuery query;
+
+        public GetBeanOfTheDayController(IGetBeanOfTheDayQuery query)
+        {
+            this.query = query;
+        }
+
+        // GET api/<GetBeanOfTheDayController>
+        [HttpGet]
+        public async Task<GetBeanOfTheDayControllerResponse> GetAsync()
+        {
+            GetBeanOfTheDayQueryResponse queryResponse = await this.query.ExecuteAsync();
+            return GetBeanOfTheDayControllerResponse.FromQueryResponse(queryResponse);
+        }
+    }
+}
---
+++ src/all-the-beans.Api/Controllers/V1/CoffeeBeanControllers/GetBeanOfTheDay/GetBeanOfTheDayControllerResponse.cs
@@ -0,0 +1,24 @@
+﻿using all_the_beans.Entities.Queries.V1.GetBeanOfTheDay;
+
+namespace all_the_beans.Api.Controllers.V1.CoffeeBeanControllers.GetBeanOfTheDay
+{
+    public partial record GetBeanOfTheDayControllerResponse(string Id, decimal Cost, string Image, string Name, string Colour, string Description, string Country)
+    {
+    }
+
+    public partial record GetBeanOfTheDayControllerResponse
+    {
+        public static GetBeanOfTheDayControllerResponse FromQueryResponse(GetBeanOfTheDayQueryResponse queryResponse)
+        {
+            return new GetBeanOfTheDayControllerResponse(
+                Id: queryResponse.Id,
+                Cost: queryResponse.Cost,
+                Image: queryResponse.Image,
+                Name: queryResponse.Name,
+                Colour: queryResponse.Colour,
+                Description: queryResponse.Description,
+                Country: queryResponse.Country
+            );
+        }
+    }
+}
