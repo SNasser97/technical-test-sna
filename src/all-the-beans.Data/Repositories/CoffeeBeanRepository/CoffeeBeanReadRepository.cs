e4b9b3b14ab1d4393499360e43e5041781690256
Syed Nasser Ahmed <SNasserAhmed@outlook.com>
2025-05-06 13:20:26 +0100

    feat: implement read repository filtering - GetCoffeeBeans

--- src/all-the-beans.Data/Repositories/CoffeeBeanRepository/CoffeeBeanReadRepository.cs
+++ src/all-the-beans.Data/Repositories/CoffeeBeanRepository/CoffeeBeanReadRepository.cs
@@ -3,6 +3,7 @@ using all_the_beans.Data.Tables.CoffeeBeanTable;
 using all_the_beans.Entities.Entity.CoffeeBean;
 using all_the_beans.Entities.Repositories.CoffeeBeanRepository;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace all_the_beans.Data.Repositories.CoffeeBeanRepository
 {
@@ -18,15 +19,42 @@ namespace all_the_beans.Data.Repositories.CoffeeBeanRepository
         /// <inherit doc/>
         public async Task<IEnumerable<CoffeeBean>> GetAsync(int pageNumber, int itemsPerPage)
         {
-   
-            IEnumerable<CoffeeBeanTable> coffeeBeanTableRecords = await this.coffeeBeanDbContext.CoffeeBean
-                    .OrderBy(p => p.Index)
-                    .Skip((pageNumber - 1) * itemsPerPage)
-                    .Take(itemsPerPage)
-                    .AsNoTracking()
-                    .ToListAsync();
+            // pass empty dictionary to avoid null check in GetAsync(int, int, IDictionary<string,string> filters
+            return await this.GetAsync(pageNumber, itemsPerPage);
+        }
 
-            return coffeeBeanTableRecords.Select(coffeeBeanTableRecord => CoffeeBeanTable.ToCoffeeBeanEntity(coffeeBeanTableRecord));
+        public async Task<IEnumerable<CoffeeBean>> GetAsync(int pageNumber, int itemsPerPage, IDictionary<string,string> filters=null)
+        {
+            filters ??= new Dictionary<string, string>();
+
+            IQueryable<CoffeeBeanTable> query = this.coffeeBeanDbContext.CoffeeBean;
+
+            if (filters.Any())
+            {
+                query = this.GetFilters(query, filters);
+            }
+
+            IEnumerable<CoffeeBeanTable> coffeeBeanTableRecords = await query
+               .OrderBy(p => p.Index)
+               .Skip((pageNumber - 1) * itemsPerPage)
+               .Take(itemsPerPage)
+               .AsNoTracking()
+               .ToListAsync();
+
+            return coffeeBeanTableRecords.Select(CoffeeBeanTable.ToCoffeeBeanEntity);
+        }
+
+        private IQueryable<CoffeeBeanTable> GetFilters(IQueryable<CoffeeBeanTable> query, IDictionary<string, string> filters)
+        {
+            foreach (KeyValuePair<string,string> filter in filters)
+            {
+                if (!string.IsNullOrEmpty(filter.Value))
+                {
+                    query = query.Where(p => EF.Property<string>(p, filter.Key).StartsWith(filter.Value));
+                }
+            }
+
+            return query;
         }
     }
 }
